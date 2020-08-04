06a6a31afdabbf1e4e813e296bc233d3cffb3157
Murat Baltacioglu <murat.baltacioglu@semine.no>
2020-08-04 04:28:47 +0200

    native project name changed prevent nuget package name conflict

--- Murat.CircleProgressBar.Droid/Properties/AssemblyInfo.cs
+++ Murat.CircleProgressBar.Droid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Naxam.CircleProgressBar.Droid")]
+[assembly: AssemblyTitle("Murat.CircleProgressBar.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Naxam.CircleProgressBar.Droid")]
+[assembly: AssemblyProduct("Murat.CircleProgressBar.Droid")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
